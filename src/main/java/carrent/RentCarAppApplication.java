33888460d16d23feb211afed92b721832991b70a
MateuszRostek <17rostek17@gmail.com>
2024-04-22 00:18:24 +0200

    Infrastructure setup

---
+++ src/main/java/carrent/RentCarAppApplication.java
@@ -0,0 +1,13 @@
+package carrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentCarAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentCarAppApplication.class, args);
+    }
+
+}
