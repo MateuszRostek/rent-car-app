37efc5948b3092128161321d13627c630ec606d5
MateuszRostek <17rostek17@gmail.com>
2024-04-25 20:30:58 +0200

    Introduced Spring Security (Config, UserDetails, UserDetailsService) and roles

---
+++ src/main/java/carrent/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package carrent.config;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final UserDetailsService userDetailsService;
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST,
+                                "/auth/registration")
+                        .permitAll()
+                        .requestMatchers(HttpMethod.GET,
+                                "/cars/**")
+                        .permitAll()
+                        .requestMatchers("/swagger-ui/**")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+                )
+                .httpBasic(withDefaults())
+                .userDetailsService(userDetailsService)
+                .build();
+    }
+}
--- src/main/java/carrent/repository/user/UserRepository.java
+++ src/main/java/carrent/repository/user/UserRepository.java
@@ -3,9 +3,11 @@ package carrent.repository.user;
 import carrent.model.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("from User u join fetch u.roles where lower(u.email) like lower(:email)")
     Optional<User> findByEmail(String email);
 }
