95eaaca1b83984bacb67b42f281efd723cadb8db
MateuszRostek <17rostek17@gmail.com>
2024-04-26 21:28:16 +0200

    Added encoding password when registering new User and set default customer role

--- src/main/java/carrent/repository/role/RoleRepository.java
+++ src/main/java/carrent/repository/role/RoleRepository.java
@@ -1,9 +1,11 @@
 package carrent.repository.role;
 
 import carrent.model.Role;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(Role.RoleName name);
 }
