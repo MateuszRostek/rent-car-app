544785e38a408ece0f694bdab13afcb874fff76e
MateuszRostek <17rostek17@gmail.com>
2024-04-29 16:35:56 +0200

    Implemented Rental model and converter for proper time conversion, added Liquibase support for creating rental table

---
+++ src/main/java/carrent/model/Rental.java
@@ -0,0 +1,38 @@
+package carrent.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "rentals")
+public class Rental {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "rental_date", nullable = false)
+    private LocalDate rentalDate;
+    @Column(name = "return_date", nullable = false)
+    private LocalDate returnDate;
+    @Column(name = "actual_return_date")
+    private LocalDate actualReturnDate;
+    @ManyToOne
+    @JoinColumn(name = "car_id")
+    private Car car;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
