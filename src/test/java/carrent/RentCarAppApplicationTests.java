f4e9ef56e5cd8b74fca2cbed443b0ec7a8b8fcb7
MateuszRostek <17rostek17@gmail.com>
2024-05-16 00:33:03 +0200

    Removed contextLoad test

--- src/test/java/carrent/RentCarAppApplicationTests.java
+++ src/test/java/carrent/RentCarAppApplicationTests.java
@@ -1,13 +1,8 @@
 package carrent;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RentCarAppApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
